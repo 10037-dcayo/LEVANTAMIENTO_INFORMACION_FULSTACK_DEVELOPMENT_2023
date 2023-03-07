f2f9f57a617a2a593f5b60ac8448935b62e37087
unknown <jgmejia2@espe.edu.ec>
2023-03-07 17:43:57 -0500

    Update conexion.php

--- v2/Sistema-LI/modules/conexion.php
+++ v2/Sistema-LI/modules/conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Guayaquil');
 
-$conexion = mysqli_connect("localhost", "root", "Minerales99", "db_school");
+$conexion = mysqli_connect("localhost", "root", "1234", "db_school");
 
 
 if (mysqli_connect_errno()) {
