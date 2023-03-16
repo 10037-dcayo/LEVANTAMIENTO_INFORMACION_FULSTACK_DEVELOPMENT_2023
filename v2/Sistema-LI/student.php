4d730cec0aca9eb3874ab69be14d372ceb7f9f93
Michael23-OSS <66226118+Michael23-OSS@users.noreply.github.com>
2023-03-16 15:43:19 -0500

    PAGES

---
+++ v2/Sistema-LI/student.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">
+<title>Modulo Estudiante</title>
+<meta name="Description" content="Paginas donde encontrar contenido interesante">
+<link rel="canonical" href="https://misitio/index.php">
+<meta name="Robots" content="index, follow">
+<link href="./css/estilo.css" rel="stylesheet" type="text/css" media="screen">
+</head> 
+<body>
+<div id="inicio"></div>
+<div id="header">
+<div id="bar">
+<a href="https://misitio/index.php"><title>Modulo Estudiante</title></a>
+<div class="descrip"></div>
+</div>
+<div id="page">
+<h1>Bienvenido al Modulo Estudiante</h1>
+<div class="intro">
+Descripción del Modulo Estudiante.
+</div>
+Esta es la página principal o de inicio de mi sitio web.<br>
+Es la primera que ven los que entran al sitio por primera vez y en todas las páginas restantes hay un enlace que conduce a ella.<br>
+En ella puedo poner enlaces a las otras páginas, imágenes sugestivas, videos o cualquier otro elemento.<br>
+<img src="./images/background_m_estudiantes.jpg" width="620" height="400" alt="Imagen para el intro" title="Mi sitio web, actualidad y tecnologia"><br>
+
+<h2>ENLACES A LOS CAMPUS VIRTUALES ESPE</h2>
+<div class="enlaces">
+<a href= "https://srvcas.espe.edu.ec/authenticationendpoint/login.do?Name=PreLoginRequestProcessor&commonAuthCallerPath=%252Fcas%252Flogin&forceAuth=true&passiveAuth=false&service=https%3A%2F%2Fmiespe.espe.edu.ec%2Fc%2Fportal%2Flogin&tenantDomain=carbon.super&sessionDataKey=df395a49-5b4c-4714-a543-090dd30de787&relyingParty=portal_luminis&type=cas&sp=portal_luminis&isSaaSApp=false&authenticators=BasicAuthenticator:LOCAL" target="_blank">MI ESPE</a><br>
+<a href="https://moodleltga.espe.edu.ec/moodle/" target="_blank">MOODLE</a><br>
+</div>
+
+Puede tener cualquier cantidad de texto, pero se recomienda que sea breve, que no sea necesario desplazarse por ella, que el contenido principal pueda verlo el lector al primer vistazo.<br>
+En la parte inferior del título están los botones para compartirla en las principales redes sociales.<br>
+En el extremo inferior deben ir los botones que permitan a los lectores seguirnos en estos sitios sociales.<br>
+<br>
+
+<h3>Sígueme en las redes sociales</h3>
+<a href="http://www.facebook.com/" rel="nofollow">
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 32 32"><path fill="#3B5998" d="M0 0h32v32H0z"/><path fill="#FFF" d="M22.08 32V19.607h4.16l.62-4.83h-4.78v-3.083c0-1.398.388-2.352 2.393-2.352h2.56V5.02c-.443-.058-1.962-.19-3.728-.19-3.688 0-6.213 2.25-6.213 6.385v3.562h-4.17v4.83h4.17V32h4.987z"/></svg>
+</a>
+<a href="https://twitter.com/" rel="nofollow"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 32 32"><path fill="#55ACEE" d="M0 0h32v32H0z"/><path fill="#FFF" d="M28 8.557c-.884.39-1.833.656-2.828.775 1.017-.608 1.798-1.573 2.166-2.725-.953.567-2.006.976-3.13 1.194-.896-.956-2.176-1.554-3.593-1.554-2.72 0-4.924 2.206-4.924 4.925 0 .387.045.763.13 1.125-4.094-.208-7.724-2.168-10.15-5.147-.423.726-.667 1.573-.667 2.476 0 1.71.87 3.214 2.19 4.1-.806-.027-1.564-.25-2.23-.616v.06c0 2.39 1.7 4.378 3.952 4.83-.414.113-.85.172-1.297.172-.317 0-.625-.03-.927-.086.63 1.956 2.447 3.38 4.6 3.42-1.685 1.318-3.808 2.107-6.114 2.107-.398 0-.79-.023-1.175-.068 2.18 1.396 4.768 2.213 7.55 2.213 9.056 0 14.01-7.506 14.01-14.012 0-.213-.005-.426-.015-.637.96-.694 1.795-1.56 2.455-2.55z"/></svg>
+</a><br>
+
+<div class="subir"><a href="#inicio">Subir</a></div>
+<div class="footer">
+Universidad de las Fuerzas Armadas ESPE &copy; 2023 <a href="https://prowessec.com/" target="_blank">PROWESSEC.EC</a><br>
+</div><br>
+</div></div>
+</body> 
+</html>
