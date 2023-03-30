899cba89e25a600779dbd451e5e55aff6a0d34d6
Ricardo Alejandro Jaramillo Salgado <nrjaramillo2@espe.edu.ec>
2023-03-30 10:30:41 -0500

    Update load_data.php

--- v2/Sistema-LI/modules/students/load_data.php
+++ v2/Sistema-LI/modules/students/load_data.php
@@ -31,14 +31,18 @@ if ($result = $conexion->query($sql)) {
 
 				$_SESSION['student_email'] = array();
 
+				$_SESSION['student_status'] = array();
+
+				$_SESSION['student_jerarquia'] = array();
 
 				$_SESSION['student_name'] = array();
 				$_SESSION['student_cedula'] = array();
 				$_SESSION['student_date'] = array();
+				$_SESSION['student_documentation'] = array();
 
 				$i = 0;
 
-				$sql = "SELECT * FROM students WHERE user LIKE '%" . $_POST['search'] . "%' OR email LIKE '%". $_POST['search'] . "%' OR name LIKE '%" . $_POST['search'] . "%' OR surnames LIKE '%" . $_POST['search'] . "%' OR cedula LIKE '%" . $_POST['search'] . "%' OR admission_date LIKE '%" . $_POST['search'] . "%' OR departamento LIKE '%" . $_POST['search'] . "%' ORDER BY name";
+				$sql = "SELECT * FROM students WHERE user LIKE '%" . $_POST['search'] . "%' OR email LIKE '%". $_POST['search'] . "%' OR name LIKE '%" . $_POST['search'] . "%' OR surnames LIKE '%" . $_POST['search'] . "%' OR cedula LIKE '%" . $_POST['search'] . "%' OR admission_date LIKE '%" . $_POST['search'] . "%' OR documentation LIKE '%" . $_POST['search'] . "%' OR estado LIKE '%" . $_POST['search'] . "%' OR jerarquia LIKE '%" . $_POST['search'] . "%' OR departamento LIKE '%" . $_POST['search'] . "%' ORDER BY name";
 
 				if ($result = $conexion->query($sql)) {
 					while ($row = mysqli_fetch_array($result)) {
@@ -48,6 +52,9 @@ if ($result = $conexion->query($sql)) {
 						$_SESSION['student_date'][$i] = $row['admission_date'];
 						$_SESSION['student_email'][$i] = $row['email'];
 						$_SESSION['student_departamento'][$i] = $row['departamento'];
+						$_SESSION['student_documentation'][$i] = $row['documentation'];
+						$_SESSION['student_status'][$i] = $row['estado']; 
+						$_SESSION['student_jerarquia'][$i] = $row['jerarquia'];
 
 
 
@@ -61,8 +68,10 @@ if ($result = $conexion->query($sql)) {
 				$_SESSION['student_cedula'] = array();
 				$_SESSION['email'] = array();
 				$_SESSION['student_departamento'] = array();
-
+				$_SESSION['student_documentation'] = array();
 				$_SESSION['student_date'] = array();
+				$_SESSION['student_status'] = array();
+				$_SESSION['student_jerarquia'] = array();
 
 				$i = 0;
 
@@ -76,6 +85,10 @@ if ($result = $conexion->query($sql)) {
 						$_SESSION['email'][$i] = $row['email'];
 						$_SESSION['student_date'][$i] = $row['admission_date'];
 						$_SESSION['student_departamento'][$i] = $row['departamento'];
+						$_SESSION['student_documentation'][$i] = $row['documentation'];
+						$_SESSION['student_status'][$i] = $row['estado'];
+						$_SESSION['student_jerarquia'][$i] = $row['jerarquia'];
+
 
 
 						$i += 1;
