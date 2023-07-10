6cb4f97063a640f9373998bee4083e82b578b68a
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-07-10 17:19:02 -0500

    Delete

--- v4/Sistema_LI/modules/send_one/delete.php
+++ v4/Sistema_LI/modules/send_one/delete.php
@@ -3,37 +3,23 @@ include_once '../security.php';
 include_once '../conexion.php';
 include_once '../notif_info_msgbox.php';
 
+
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
 if (empty($_POST['txtuserid'])) {
-	header('Location: /');
-	exit();
+    header('Location: /');
+    exit();
 }
 
-$sql_delete = "DELETE FROM users WHERE user = '" . $_POST['txtuserid'] . "'";
+$nombreArchivo = $_POST['txtuserid'];
+$rutaArchivo = 'sendonepdf/' . $_SESSION["user"] . '/' . $nombreArchivo;
 
-if (mysqli_query($conexion, $sql_delete)) {
-	$sql_delete = "DELETE FROM students WHERE user = '" . $_POST['txtuserid'] . "'";
-
-	if (mysqli_query($conexion, $sql_delete)) {
-		Error('Archivo eliminado.');
-	} else {
-		Error('Error al eliminar.');
-	}
+if (file_exists($rutaArchivo)) { 
+    if (unlink($rutaArchivo)) {
+        echo 'El archivo "' . $nombreArchivo . '" ha sido eliminado correctamente.';
+    } else {
+        echo 'No se pudo eliminar el archivo.';
+    }
 } else {
-	Error('Error al eliminar.');
+    echo 'El archivo "' . $nombreArchivo . '""' . $rutaArchivo . '" no existe.';
 }
-header('Location: /modules/send_one');
-exit();
-
-
-
-
-# ⚠⚠⚠ DO NOT DELETE ⚠⚠⚠
-
-// Todos los derechos reservados © Quito - Ecuador || Estudiantes TIC's en línea || Levantamiento de Información || ESPE 2022-2023
-
-// Ricardo Alejandro  Jaramillo Salgado, Michael Andres Espinosa Carrera, Steven Cardenas, Luis LLumiquinga
-
-# ⚠⚠⚠ DO NOT DELETE ⚠⚠⚠
-
