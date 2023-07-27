d1c40e977cc5c5256698fa98523274757af63e2a
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-07-27 17:39:15 -0500

    Update

--- v4/Sistema_LI/modules/edit_send_one/form_update.php
+++ v4/Sistema_LI/modules/edit_send_one/form_update.php
@@ -1,9 +1,4 @@
 <?php
-include_once '../security.php';
-include_once '../conexion.php';
-include_once '../notif_info_msgbox.php';
-
-
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
 $sql = "SELECT * FROM send_one WHERE archivopdf = '" . $_POST['txtuserid'] . "'";
@@ -20,14 +15,14 @@ if ($result = $conexion->query($sql)) {
 
 ?>
 <div class="form-data">
-  <div class="head">
-    <h1 class="titulo">Revisión de Documentación</h1>
-  </div>
-  <div class="body">
-    <form action="#" method="post" autocomplete="off" autocapitalize="on" enctype="multipart/form-data">
-      <div class="wrap">
-        <div class="first">
-          <label for="txtuserid" <label for="txtuserid" class="label">Usuario</label>
+	<div class="head">
+		<h1 class="titulo">Actualizar</h1>
+	</div>
+	<div class="body">
+		<form name="form-update-students" action="update.php" method="POST" autocomplete="off" autocapitalize="on">
+			<div class="wrap">
+      <div class="first">
+          <label for="txtuserid" class="label">Usuario</label>
           <input id="txtuserid" style="display: none;" type="text" name="userid"
             value="<?php echo $_SESSION['user_id']; ?>" maxlength="50">
           <input class="text" type="text" name="txt" value="<?php echo $_SESSION['user_id']; ?>" maxlength="50"
@@ -54,18 +49,34 @@ if ($result = $conexion->query($sql)) {
             value="<?php echo $_SESSION['numero']; ?>" maxlength="50" required />
           <input class="text" type="text" name="txt" value="<?php echo $_SESSION['numero']; ?>" required disabled />
         </div>
-
-      </div>
-      <form action="" method="POST">
-        <input style="display:none;" type="text" name="txtuserid" value="' . $_SESSION[" user_id"][$i] . '"/>
-                <button class="btn icon" name="btn" value="form_delete" autofocus type="submit">done</button>
-              </form>
-    </form>
-  </div>
+</div>
+			<button id="btnSave" class="btn icon" type="submit">save</button>
+		</form>
+	</div>
 </div>
 <div class="content-aside">
-  <?php
-  include_once "../sections/options-disabled.php";
-  ?>
+	<?php include_once "../sections/options-disabled.php"; ?>
 </div>
+
+
 <script src="/js/modules/students.js" type="text/javascript"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-0sCz7O9XlHUBlTepQg2tL/j/ZtMInzGRBfKv2n/bGEB1MkXkXpy0eMHvG+vcnBfACpJZl+S6Z5p5r5L5Hy5U2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
+
+
+<?php
+
+# ⚠⚠⚠ DO NOT DELETE ⚠⚠⚠
+
+// Todos los derechos reservados © Quito - Ecuador || Estudiantes TIC's en línea || Levantamiento de Información || ESPE 2022-2023
+
+// Ricardo Alejandro  Jaramillo Salgado, Michael Andres Espinosa Carrera, Steven Cardenas, Luis LLumiquinga
+
+# ⚠⚠⚠ DO NOT DELETE ⚠⚠⚠
+
+?>
