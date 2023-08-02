d6af51e5ce7b7025686abb78bded016e9f2a8ba3
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-08-02 17:55:59 -0500

    Commit

--- v4/Sistema_LI/modules/edit_send_one/menu.php
+++ v4/Sistema_LI/modules/edit_send_one/menu.php
@@ -1,22 +1,23 @@
 <?php
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
-
+$name=$_POST['txtname'];
+$id=$_POST['txtuserid'];
 
 ?>
 
 <div class="content-aside">
-	<?php
-	include_once '../notif_info.php';
-	include_once "../sections/options.php";
-	?>
+  <?php
+  include_once '../notif_info.php';
+  include_once "../sections/options.php";
+  ?>
 </div>
 
 <div class="form-gridview">
   <table class="default">
-		<?php 
-			echo '<h2 class="textList"> ' .$_POST['txtname'].' </h2>'
-		?>
+    <?php
+    echo '<h2 class="textList"> ' . $_POST['txtname'] . ' </h2>';
+      ?>
     <div class="textList">
       <div class="item upLeft">
         <h2 class="textList">Justificación</h2>
@@ -32,9 +33,12 @@ require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php
       </div>
       <div class="item downLeft">
         <h2 class="textList">Envío 1</h2>
-        <form action="form-name-repetido-3.php">
-          <p><input type="submit" value="Ver"></p>
-        </form>
+
+        <form action="" method="POST">
+        <input  style="display:none;" type="text" id="texuserid" name="txtuserid"  value="<?php echo $id; ?>"/>
+        <input  style="display:none;" type="text" id="texname" name="txtname"  value="<?php echo $name; ?>"/>
+																<button class="btn" id="btnSave" value="form_documents" name="btn" type="submit">Ver</button>
+															</form>
       </div>
       <div class="item downRight">
         <h2 class="textList">Envío 2</h2>
