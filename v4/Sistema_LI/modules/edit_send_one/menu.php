1fe9bfb9336b6bc78a1fcd7dfe6e2f2aa8ba6f0d
Rubén Darío González Gudiño <rdgonzalez4@espe.edu.ec>
2023-08-02 16:04:55 -0500

    menu

--- v4/Sistema_LI/modules/edit_send_one/menu.php
+++ v4/Sistema_LI/modules/edit_send_one/menu.php
@@ -5,17 +5,43 @@ require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php
 
 ?>
 
-<div class="form-gridview">
-<table class="default">
-		<?php 
-			echo '<h2 class="textList"> ' .$_POST['txtname'].' </h2>'
-		?>
-  </table>  
-</div>
-
 <div class="content-aside">
 	<?php
 	include_once '../notif_info.php';
 	include_once "../sections/options.php";
 	?>
+</div>
+
+<div class="form-gridview">
+  <table class="default">
+		<?php 
+			echo '<h2 class="textList"> ' .$_POST['txtname'].' </h2>'
+		?>
+    <div class="textList">
+      <div class="item upLeft">
+        <h2 class="textList">Justificación</h2>
+        <form action="form-name-repetido-1.php">
+          <p><input type="submit" value="Ver"></p>
+        </form>
+      </div>
+      <div class="item upRight">
+        <h2 class="textList">Informes Quincenales</h2>
+        <form action="form-name-repetido-2.php">
+          <p><input type="submit" value="Ver"></p>
+        </form>
+      </div>
+      <div class="item downLeft">
+        <h2 class="textList">Envío 1</h2>
+        <form action="form-name-repetido-3.php">
+          <p><input type="submit" value="Ver"></p>
+        </form>
+      </div>
+      <div class="item downRight">
+        <h2 class="textList">Envío 2</h2>
+        <form action="form-name-repetido-4.php">
+          <p><input type="submit" value="Ver"></p>
+        </form>
+      </div>
+    </div
+  </table>
 </div>
