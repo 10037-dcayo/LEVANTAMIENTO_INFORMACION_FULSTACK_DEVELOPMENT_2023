feeb43d8208cbca0a526478fe83258829d1c0b0a
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-08-03 17:58:30 -0500

    New

--- v4/Sistema_LI/modules/edit_send_one/menu.php
+++ v4/Sistema_LI/modules/edit_send_one/menu.php
@@ -19,26 +19,14 @@ $id = $_POST['txtuserid'];
     echo '<h2 class="textList"> ' . $_POST['txtname'] . ' </h2>';
     ?>
     <div class="textList">
-<<<<<<< HEAD
-    <div class="textList">
-      <div class="item upLeft rounded-blue-box">
-        <div class="flex-container">
-          <h2 class="textList">Justificación</h2>
-          <form action="form-name-repetido-1.php">
-            <p><input type="submit" value="Ver"></p>
-          </form>
-        </div>
-=======
     <div class="item downLeft rounded-blue-box">
-        <h2 >Justificaciónes</h2>
+        <h2>Justificaciónes</h2>
         <form action="" method="POST">
           <input style="display:none;" type="text" id="texuserid" name="txtuserid" value="<?php echo $id; ?>" />
           <input style="display:none;" type="text" id="texname" name="txtname" value="<?php echo $name; ?>" />
           <button class="btn" id="btnSave" value="form_justificaciones" name="btn" type="submit">Ver</button>
         </form>
->>>>>>> 88a4d1f4e1ee60d1e08a78a7cb5bec273063b7c8
       </div>
-    </div>
       <div class="item upRight rounded-blue-box">
         <h2 >Informes Quincenales</h2>
         <form action="form-name-repetido-2.php">
