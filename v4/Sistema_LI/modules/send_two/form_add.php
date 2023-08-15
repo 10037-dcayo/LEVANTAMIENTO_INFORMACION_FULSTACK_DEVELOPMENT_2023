f349fa031cd3486ed9c41bfb04dbcc99d9bb0673
Luis Tavaris <latavaris@espe.edu.ec>
2023-08-15 17:45:20 -0500

    sentencia

--- v4/Sistema_LI/modules/send_two/form_add.php
+++ v4/Sistema_LI/modules/send_two/form_add.php
@@ -39,7 +39,7 @@ $id_generate = 'S-' . unique_id(5);
                     </div>
                     <div class="first">
                     <label for="txtuserarchivo" class="label">Archivo</label>
-                    <input type="file" class="text" id="archivo" name="archivo" accept="application/pdf" required>
+                    <input type="file" class="text" id="archivo" name="archivo" accept="application/pdf , application/msword " required>
                     </div>
                     </div>
             
