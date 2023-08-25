e2b18245023bbf8dac0248f5c172a652b0bd73e0
Esteban Mateo Loachamin Aldaz <107655020+EstebanLoa@users.noreply.github.com>
2023-08-25 18:13:52 -0500

    form

--- v4/Sistema_LI/modules/Informes_Quincenales/insert.php
+++ v4/Sistema_LI/modules/Informes_Quincenales/insert.php
@@ -33,9 +33,10 @@ $nombrePDF=$_SESSION['infoq_archivo'];
 	$numeroDePDF = $_POST['num'];
 	$descripcion = $_POST['descripcion'];
 	$date = date('Y-m-d H:i:s');
+	$status="En revisión";
+	$mensaje="Sin comentarios";
 	
-	
-	$sql = "INSERT INTO infoq (user, num, archivopdf, descripcion, created_at, updated_at) VALUES ('$usuario', '$numeroDePDF', '$archivopdf', '$descripcion', '$date', '$date')";
+	$sql = "INSERT INTO infoq (user, num, archivopdf, descripcion, created_at, updated_at, estado, message) VALUES ('$usuario', '$numeroDePDF', '$archivopdf', '$descripcion', '$date', '$date','$status', '$mensaje')";
 	$resultado = $conexion->query($sql);
     $id = $_SESSION["user_id"];
     echo "Mi id es: " . $id;
--- v4/Sistema_LI/modules/edit_send_one/form_default.php
+++ v4/Sistema_LI/modules/edit_send_one/form_default.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
