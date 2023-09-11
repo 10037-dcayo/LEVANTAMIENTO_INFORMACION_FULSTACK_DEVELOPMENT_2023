f20a0a1329d0914090c324d73b828c553ac7caa1
EstebanLoa <107655020+EstebanLoa@users.noreply.github.com>
2023-09-11 10:06:40 -0500

    New

--- v4/Sistema_LI/modules/students/form_update.php
+++ v4/Sistema_LI/modules/students/form_update.php
@@ -158,22 +158,26 @@ if ($result = $conexion->query($sql)) {
 					<label for="selectuserdepartamento" class="label">Departamento</label>
 					<select id="selectuserdepartamento" class="select" name="selectDepartamento" required>
 					    <option value="<?php echo $_SESSION['student_departamento']; ?>"><?php echo $_SESSION['student_departamento']; ?></option>
-                        <option value="SOFTWARE - PROWESS EC PÁGINA WEB VENTAS">SOFTWARE - PROWESS EC PÁGINA WEB VENTAS</option>
+							<option value="SOFTWARE - PROWESS EC PÁGINA WEB VENTAS">SOFTWARE - PROWESS EC PÁGINA WEB VENTAS</option>
                         <option value="CONTABILIDAD Y AUDITORIA MAÑANA">CONTABILIDAD Y AUDITORIA MAÑANA</option>
-                        <option value="INVESTIGACIÓN">INVESTIGACIÓN</option>
-                        <option value="DOCUMENTACIÓN">DOCUMENTACIÓN</option>
+                        <option value="INVESTIGACIÓN">DEPARTAMENTO DE INVESTIGACIÓN</option>
+                        <option value="DOCUMENTACIÓN">DEPARTAMENTO DE DOCUMENTACIÓN</option>
                         <option value="ADMINISTRACIÓN DE EMPRESAS - TALLER">ADMINISTRACIÓN DE EMPRESAS - TALLER</option>
                         <option value="MARKETING">MARKETING</option>
-                        <option value="CREACIÓN DE CONTENIDO">CREACIÓN DE CONTENIDO</option>
+                        <option value="CREACIÓN DE CONTENIDO">DEPARTAMENTO DE CONTENIDO</option>
                         <option value="MENTORIAS MATUTINO">MENTORIAS MATUTINO </option>
                         <option value="SOFTWARE - PROWESS BIKE">SOFTWARE - PROWESS BIKE</option>
-                        <option value="CONTABILIDAD Y AUDITORÍA/ TARDE">CONTABILIDAD Y AUDITORÍA/ TARDE</option>
+                        <option value="CONTABILIDAD Y AUDITORÍA/ TARDE">FINAZAS Y CONTABILIDAD/ TARDE</option>
                         <option value="SOFTWARE - DESARROLLO PÁGINA WEB PROWESS AGRÍCOLA">SOFTWARE - DESARROLLO PÁGINA WEB PROWESS AGRÍCOLA</option>
                         <option value="SOFTWARE - PROWESS APP AGRÍCOLA">SOFTWARE - PROWESS APP AGRÍCOLA</option>
                         <option value="MODULO CURSOS MOCC">MODULO CURSOS MOCC</option>
                         <option value="MENTORIAS VESPERTINO">MENTORIAS VESPERTINO</option>
                         <option value="LEVANTAMIENTO DE INFORMACIÓN">LEVANTAMIENTO DE INFORMACIÓN</option>
                         <option value="ELABORACIÓN DE MATERIALES DE APOYO">ELABORACIÓN DE MATERIALES DE APOYO</option>
+                        <option value="ADMINISTRACION II">ADMINISTRACIÓN II</option>
+                        <option value="ADMINISTRACION I">ADMINISTRACIÓN I</option>
+                        <option value="PREINCUBACIÓN">PREINCUBACIÓN</option>
+                        <option value="PROWESS VENTAS">PROWESS VENTAS</option>
 					</select>
 
 				</div>
