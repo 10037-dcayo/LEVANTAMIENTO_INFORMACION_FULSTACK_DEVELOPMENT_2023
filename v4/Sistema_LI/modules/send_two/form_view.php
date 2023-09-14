a07607da9c5bd9ab114eca89454770610ec3e72e
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-09-14 11:01:56 -0500

    Se agrega el campo comentario y se reestructura el form

--- v4/Sistema_LI/modules/send_two/form_view.php
+++ v4/Sistema_LI/modules/send_two/form_view.php
@@ -14,13 +14,15 @@ if ($result = $conexion->query($sql)) {
     $_SESSION['mensaje'] = $row['message'];
     $_SESSION['nombre'] = $row['archivopdf'];
     $_SESSION['evidencia'] = $row['evidencepdf'];
+    $_SESSION['coment'] = $row['message_student'];
   }
 }
 $id = $_SESSION['user_id']; 
+//obtenemos los comentarios del estudiante
+$comenario_estudiante=$_SESSION['coment'];
 // Obtén el nombre del archivo desde la base de datos o alguna otra fuente
 $nombre_del_archivo = $_SESSION['evidencia'];
 // Construye la URL completa al archivo PDF
-
 $url_archivo_pdf = '/modules/edit_send_one/sendtwopdf/' . $id . '/' . $nombre_del_archivo;
 
 
@@ -40,15 +42,22 @@ $url_archivo_pdf = '/modules/edit_send_one/sendtwopdf/' . $id . '/' . $nombre_de
             value="<?php echo $_SESSION['user_id']; ?>" maxlength="50">
           <input class="text" type="text" name="txt" value="<?php echo $_SESSION['user_id']; ?>" maxlength="50"
             disabled />
-          <label for="txtinfoqdescription" class="label">Descripción</label>
-          <textarea name="descripcion" id="descripcion" class="textarea" cols="30" rows="10"
-            value="<?php echo $_SESSION['user_id']; ?>" readonly><?php echo $_SESSION['mensaje']; ?></textarea>
         </div>
         <div class="first">
           <label for="txtname" class="label">Estado</label>
           <input id="txtname" class="text" style=" display: none;" type="text" name="name"
             value="<?php echo $_SESSION['state']; ?>" maxlength="50" required />
           <input class="text" type="text" name="txt" value="<?php echo $_SESSION['state']; ?>" required disabled />
+        </div>
+        <div class="first">
+        <label for="txtinfoqdescription" class="label">Comentario de documentación</label>
+          <textarea name="descripcion" id="descripcion" class="textarea" cols="30" rows="10"
+            value="<?php echo $_SESSION['user_id']; ?>" readonly><?php echo $_SESSION['mensaje']; ?></textarea>
+        </div>
+        <div class="first">
+          <label for="txtcomentario" class="label">Comentarios del estudiante</label>
+          <textarea name="comentario" id="comentario" class="textarea" cols="30" rows="10"
+            value="<?php echo $_SESSION['user_id']; ?>"><?php echo $comenario_estudiante; ?></textarea>
         </div>
         <div class="first">
           <label for="txtname" class="label">Nombre</label>
@@ -72,11 +81,7 @@ $url_archivo_pdf = '/modules/edit_send_one/sendtwopdf/' . $id . '/' . $nombre_de
         <div class="first">
           <a href="<?php echo $url_archivo_pdf; ?>" download class="btn-download">Descargar Documento</a>
         </div>
-
-
-
       </div>
-
 
       <button class="btn icon" type="submit" autofocus>done</button>
     </form>
