51a84733aa67ccd19e639f55191daa9329a7b714
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-09-15 10:17:18 -0500

    Actualización

--- v4/Sistema_LI/modules/send_one/form_add.php
+++ v4/Sistema_LI/modules/send_one/form_add.php
@@ -29,19 +29,21 @@ $id_generate = 'S-' . unique_id(5);
                     <label for="txtuserid" class="label">Usuario</label>
                     <input id="txtuserid" style="display: none;" type="text" name="userid" value="<?php echo $_SESSION['user_id']; ?>" maxlength="50">
                     <input class="text" type="text" name="txt" value="<?php echo $_SESSION['user_id']; ?>" maxlength="50" disabled />
-                    <label for="txtinfoqdescription" class="label">Descripción</label>
-                    <textarea name="descripcion" id="descripcion" class="textarea" cols="30" rows="10" readonly>Documentación</textarea>
-                    </div>
-                    <div class="first">
+                </div>
+                <div class="first">
                     <label for="txtusernum" class="label">N°PDF</label>
                     <input id="txtusernum" class="text" style=" display: none;" type="text" name="num" value="<?php echo $id_generate; ?>" maxlength="50" required />
                     <input class="text" type="text" name="txt" value="<?php echo $id_generate; ?>" required disabled />     
-                    </div>
-                    <div class="first">
+                </div>
+                <div class="first">
                     <label for="txtuserarchivo" class="label">Archivo</label>
                     <input type="file" class="text" id="archivo" name="archivo" accept="application/pdf" required>
-                    </div>
-                    </div>
+                </div>
+                <div class="first">
+                    <label for="txtinfoqdescription" class="label">Descripción</label>
+                    <textarea name="descripcion" id="descripcion" class="textarea" cols="30" rows="10" readonly>Documentación</textarea>
+                </div>
+            </div>
             
             <button class="btn icon" type="submit">save</button>
         </form>
