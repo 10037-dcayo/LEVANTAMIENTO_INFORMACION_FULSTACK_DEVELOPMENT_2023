482ee93547f9545fef49cdb7a014c41ff4f99571
Cristian Fernando Idrobo Montalvo <105193365+cfidrobo@users.noreply.github.com>
2023-09-20 11:47:15 -0500

    v4

--- v4/Sistema_LI/modules/send_one/delete.php
+++ v4/Sistema_LI/modules/send_one/delete.php
@@ -27,19 +27,44 @@ if (mysqli_query($conexion, $sql_delete)) {
 }
 
 $nombreArchivo = $_POST['txtuserid'];
-$rutaArchivo = 'sendonepdf/' . $_SESSION["user"] . '/' . $nombreArchivo;
 
-if (file_exists($rutaArchivo) & mysqli_query($conexion, $sql_delete))
- { 
-    if (unlink($rutaArchivo) & mysqli_query($conexion, $sql_delete)) {
-        Error('Archivo Eliminado');
-        
+
+// Elimina la entrada de la base de datos
+$sql_delete = "DELETE FROM send_one WHERE archivopdf = '" . $nombreArchivo . "'";
+if (mysqli_query($conexion, $sql_delete)) {
+    Info('Entrada eliminada de la base de datos.');
+} else {
+    Error('Error al eliminar la entrada de la base de datos.');
+}
+// Elimina el archivo del usuario
+$rutaArchivo = 'sendonepdf/' . $_SESSION["user"] . '/' . $nombreArchivo;
+if (file_exists($rutaArchivo) && unlink($rutaArchivo)) {
+    Info('Archivo del usuario eliminado.');
+} else {
+    Error('No se pudo eliminar el archivo del usuario.');
+}
+// Vacía la carpeta del usuario en edit_send_one/sendonepdf
+$rutaCarpetaUsuario = '../edit_send_one/sendonepdf/' . $_SESSION["user"] . '/';
+if (is_dir($rutaCarpetaUsuario)) {
+    // Obtiene una lista de archivos en la carpeta
+    $archivosUsuario = glob($rutaCarpetaUsuario . '*');
+    
+    if ($archivosUsuario) {
+        foreach ($archivosUsuario as $archivo) {
+            if (is_file($archivo) && unlink($archivo)) {
+                Info('Archivo del usuario eliminado. ' );
+            } else {
+                Error('No se pudo eliminar el archivo del usuario. ' );
+            }
+        }
     } else {
-        Error( 'No se pudo eliminar el archivo.');
+        Info('No se encontraron archivos en la carpeta del usuario.');
     }
 } else {
-    Error('El archivo no existe');
+    Info('La carpeta del usuario no existe.');
 }
+
 
 header('Location: /modules/send_one');
 exit();
+?>
