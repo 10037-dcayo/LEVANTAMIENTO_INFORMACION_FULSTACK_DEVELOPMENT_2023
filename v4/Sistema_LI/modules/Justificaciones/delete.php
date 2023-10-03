cf71e345859ff71e9788d576f1ca89e618bcbbea
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-10-03 12:10:22 -0500

    Funcionalidad de borrado

---
+++ v4/Sistema_LI/modules/Justificaciones/delete.php
@@ -0,0 +1,46 @@
+<?php
+include_once '../security.php';
+include_once '../conexion.php';
+include_once '../notif_info_msgbox.php';
+
+
+require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
+
+$nombreArchivo = $_POST['txtuserid'];
+$nombreArchivoEvidencia = $_POST['txtevidencefile'];
+
+if (!empty($_POST['txtuserid'])) {
+
+    //Elimina de la base
+    $sql_delete = "DELETE FROM justificaciones WHERE archivopdf = '" . $nombreArchivo . "'";
+    //Verifica si se borro
+    if (mysqli_query($conexion, $sql_delete)) {
+        Info('Entrada eliminada de la base de datos.');
+    } else {
+        Error('Error al eliminar la entrada de la base de datos.');
+    }
+    //Contruye la ruta del repo del usuario students
+    $rutaArchivo = 'justificacionespdf/' . $_SESSION["user"] . '/' . $nombreArchivo;
+
+    //Verifica si el archivo existe y borra 
+    if (file_exists($rutaArchivo) && unlink($rutaArchivo)) {
+        Info('Archivo del usuario eliminado.');
+    } else {
+        Error('No se pudo eliminar el archivo de evidencia en la ruta: ' . $rutaArchivo);
+    }
+}
+
+    //Borra archivo y rgistro del editor
+    if (!empty($_POST['txtevidencefile'])) {
+        //Contruye la ruta del repo del usuario editor
+        $rutaArchivoEvidencia = '../edit_send_one/editjustificacion/' . $_SESSION["user"] . '/' . $nombreArchivoEvidencia;
+        if (file_exists($rutaArchivoEvidencia) && unlink($rutaArchivoEvidencia)) {
+            Info('Archivo del usuario eliminado.');
+        } else {
+            Error('No se pudo eliminar el archivo de evidencia en la ruta: ' . $rutaArchivoEvidencia);
+        }
+    }
+
+header('Location: /modules/Justificaciones');
+exit();
+?>
