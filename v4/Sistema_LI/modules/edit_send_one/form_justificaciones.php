f903067dd487469923336e71c2758623aac7f3b3
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-10-04 09:57:48 -0500

    V4

--- v4/Sistema_LI/modules/edit_send_one/form_justificaciones.php
+++ v4/Sistema_LI/modules/edit_send_one/form_justificaciones.php
@@ -6,11 +6,11 @@ $sql = "SELECT * FROM justificaciones";
 
 if ($result = $conexion->query($sql)) {
 	if ($row = mysqli_fetch_array($result)) {
-		$_SESSION['justificaciones_estado'] = $row['estado'];
-		$_SESSION['justificaciones_creado'] = $row['created_at'];
-		$_SESSION['justificaciones_actualizado'] = $row['updated_at'];
+	  $_SESSION['send_estado'] = $row['estado'];
+	  $_SESSION['send_created'] = $row['created_at'];
+	  $_SESSION['send_updated'] = $row['updated_at'];
 	}
-}
+  }
 // Debes definir el valor de $max antes de usarlo en el cálculo de $tpages
 $max = 10; // Aquí debes proporcionar el valor apropiado
 
@@ -26,137 +26,143 @@ if (!empty($_POST['search'])) {
 	$_POST['search'] = trim($_POST['search']);
 	$_POST['search'] = mysqli_real_escape_string($conexion, $_POST['search']);
 
-	$_SESSION['user_id'] = array();
 	$_SESSION['num'] = array();
-	$_SESSION['justificaciones_archivo'] = array();
-	$_SESSION['justificaciones_estado'] = array();
-	$_SESSION['justificaciones_creado'] = array();
-	$_SESSION['justificaciones_actualizado'] = array();
+	$_SESSION['send_archivo'] = array();
+	$_SESSION['send_estado'] = array();
+	$_SESSION['send_created'] = array();
+	$_SESSION['send_updated'] = array();
 
 	$i = 0;
 
-	$sql = "SELECT * FROM justificaciones WHERE num LIKE '%" . $_POST['search'] . "%' OR archivo LIKE '%" . $_POST['search'] . "%' OR user LIKE '%" . $_POST['search'] . "%' OR num LIKE '%" . $_POST['search'] . "%' OR description LIKE '%" . $_POST['search'] . "%' ORDER BY num";
+	$sql = "SELECT * FROM justificaciones WHERE num LIKE '%" . $_POST['search'] . "%' OR archivo LIKE '%" . $_POST['search'] . "%' OR user LIKE '%" . $_POST['search'] . "%' OR descripcion LIKE '%" . $_POST['search'] . "%' ORDER BY num";
 
 	if ($result = $conexion->query($sql)) {
 		while ($row = mysqli_fetch_array($result)) {
-			$_SESSION['user_id'][$i] = $row['user'];
+										   
 			$_SESSION['num'][$i] = $row['num'];
-			$_SESSION['justificaciones_archivo'][$i] = $row['archivopdf'];
-			$_SESSION['justificaciones_estado'][$i] = $row['estado'];
-			$_SESSION['justificaciones_creado'][$i] = $row['created_at'];
-			$_SESSION['justificaciones_actualizado'][$i] = $row['updated_at'];
+			$_SESSION['send_archivo'][$i] = $row['archivopdf'];
+			$_SESSION['send_estado'][$i] = $row['estado'];
+			$_SESSION['send_created'][$i] = $row['created_at'];
+			$_SESSION['send_updated'][$i] = $row['updated_at'];
 
 			$i += 1;
 		}
 	}
-	$_SESSION['total_infoq'] = count($_SESSION['num']);
+	$_SESSION['total_send'] = count($_SESSION['num']);
 } else {
-	$_SESSION['user_id'] = array();
+								
 	$_SESSION['num'] = array();
-	$_SESSION['justificaciones_archivo'] = array();
+	$_SESSION['send_archivo'] = array();
 
 	$i = 0;
 
-	$sql = "SELECT * FROM justificaciones WHERE user='" . $_POST['txtuserid'] . "'ORDER BY num LIMIT $inicio, $max";
+	$sql = "SELECT * FROM justificaciones WHERE user = '" . $_POST['txtuserid'] . "' ORDER BY num";
 
 	if ($result = $conexion->query($sql)) {
 		while ($row = mysqli_fetch_array($result)) {
-			$_SESSION['user_id'][$i] = $row['user'];
+										   
 			$_SESSION['num'][$i] = $row['num'];
-			$_SESSION['justificaciones_archivo'][$i] = $row['archivopdf'];
+			$_SESSION['send_archivo'][$i] = $row['archivopdf'];
 
 			$i += 1;
 		}
 	}
-	$_SESSION['total_justificaciones'] = count($_SESSION['num']);
+	$_SESSION['total_send'] = count($_SESSION['num']);
 }
 ?>
 
+
+
 <div class="form-gridview">
 	<table class="default">
-		<h2 class="titlecenter"> Justificaciones </h2>
+	<h2 class="titlecenter"> Justificación </h2>
+		<?php 
+			echo '<h2 class="textList"> ' .$_POST['txtname'].' </h2>'
+		?>
 		<?php
-		echo '<h2 class="textList"> ' . $_POST['txtname'] . ' </h2>'
-			?>
-		<?php
-		if ($_SESSION['total_justificaciones'] != 0) {
+															  
+	 
+	   
+		if ($_SESSION['total_send'] != 0) {
 			echo '
 					<tr>
-						<th class="center" style="width: 800px">Nombre archivo</th>
-						<th class="center" style="width: 300px">Estado</th>
+						<th class="center" style="width: 800px">Nombre del archivo</th>
+						<th class="center" style="width: 70px">Estado</th>
 						<th class="center" style="width: 300px">Creado</th>
 						<th class="center" style="width: 300px">Actualizado</th>
-				        
-						<th class="center"><a class="icon">download</a></th>
-						<th class="center"><a class="icon">edit</a></th>
+			
+				        <th class="center"><a class="icon">download</a></th>
+								<th class="center"><a class="icon">edit</a></th>
+								
 						
 			';
-			if ($_SESSION['permissions'] != 'editor') {
-				echo '<th class="center"><a class="icon">delete</a></th>';
+			if ($_SESSION['permissions'] != 'edit') {
+				//echo '<th class="center"><a class="icon">delete</a></th>';
 			}
 			echo '	
 					</tr>
 			';
-		}
+		}	
 
-		$path = '../Justificaciones/justificacionespdf/' . $_POST["txtuserid"];
-		if (file_exists($path)) {
-			$directorio = opendir($path);
-			while ($archivo = readdir($directorio)) {
-				if (!is_dir($archivo)) {
-					echo '
-                <tr>
-                    <td>' . $archivo . '</td>
-                    <td>' . $_SESSION['justificaciones_estado'] . '</td>
-                    <td>' . $_SESSION['justificaciones_creado'] . '</td>
-                    <td>' . $_SESSION['justificaciones_actualizado'] . '</td>
-                    <td>
-                        <div data="' . $path . '/' . $archivo . '">
-                            <a href="' . $path . '/' . $archivo . '" title="Ver archivo adjunto" class="btnview" target="_blank">
-                                <button class="btnview" name="btn" value="form_consult" type="submit"></button>
-                            </a>
-                        </div>
-                    </td>
-                    <td>
-                        <form action="" method="POST">
-                            <input style="display:none;" type="text" name="txtuserid" value="' . $archivo . '"/>
-                            <button class="btnedit" name="btn" value="form_updatetwo" type="submit"></button>
-                        </form>
-                    </td>
-                </tr>';
-				}
-			}
-		}
+			$path = '../Justificaciones/justificacionespdf/' . $_POST["txtuserid"];
+                if(file_exists($path)){
+                    $directorio= opendir($path);
+                    while($archivo=readdir($directorio)){
+                        if(!is_dir($archivo)){
+                            
+													echo '
+													<tr>
+														<td>' . $archivo . '</td>
+														<td>' . $_SESSION["send_estado"] . '</td>
+														<td>' . $_SESSION["send_created"] . '</td>
+														<td>' . $_SESSION["send_updated"] . '</td>
+														<td> 
+															<div data="' . $path . '/' . $archivo . '"><a href="' . $path . '/' . $archivo . '"
+															title="Ver archivo adjunto" class="btnview" target="_blank"><button class="btnview" 
+															name="btn" value="form_consult" type="submit"></button>
+								
+							  
+														</td>
+														<td>
+															<form action="" method="POST">
+																<input style="display:none;" type="text" name="txtuserid" value="'.$archivo.'"/>
+																<button class="btnedit" name="btn" value="form_updatejustificacion" type="submit"></button>
+															</form>
+														</td>													
+													</tr>';         
+                        }
+                    }
+                }
 
 
 		?>
 	</table>
-	<?php
-	if ($_SESSION['total_justificaciones'] == 0) {
-		echo '
-				<img src="/images/404.svg" class="data-not-found" alt="404">
-		';
-	}
-	if ($_SESSION['total_justificaciones'] != 0) {
-		echo '
-				<div class="pages">
-					<ul>
-		';
-		for ($n = 1; $n <= $tpages; $n++) {
-			if ($page == $n) {
-				echo '<li class="active"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
-			} else {
-				echo '<li><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
-			}
-		}
-		echo '
-					</ul>
-				</div>
-		';
-	}
+<br></br>
+											   
+		
+																
+	
+  
+											   
+		
+					   
+		 
+	
+									 
+					 
+																																								   
+		   
+																																					
+	
+   
+		
+		  
+		  
+	
+  
 
 
-	?>
+   
 </div>
 
 <div class="content-aside">
