907aa7330c12e8ec0cc217600decb8f06b31cf48
dcayo <78748705+10037-dcayo@users.noreply.github.com>
2023-10-17 09:29:05 -0500

    V1.2

--- v4/Sistema_LI/modules/edit_send_one/form_default.php
+++ v4/Sistema_LI/modules/edit_send_one/form_default.php
@@ -68,43 +68,49 @@ de ver detalles de un estudiante y cerrar notificaciones no deseadas. -->
 	?>
 		</table>
 		<?php
-if ($_SESSION['total_users'] == 0) {
-    echo '
-        <img src="/images/404.svg" class="data-not-found" alt="404">
-    ';
-}
+		if ($_SESSION['total_users'] == 0) {
+			echo '
+				<img src="/images/404.svg" class="data-not-found" alt="404">
+			';
+		}
 
-if ($_SESSION['total_users'] != 0) {
-    echo '
-        <div class="pages">
-            <ul>
-    ';
-    
-    // Botón de flecha izquierda
-    if ($page > 1) {
-        echo '<li class="arrow-button"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . ($page - 1) . '">&larr;</button></form></li>';
-    }
+		if ($_SESSION['total_users'] != 0) {
+			echo '
+				<div class="pages">
+					<ul>
+			';
+			
+			// Botón de flecha izquierda si no está en la primera página
+			if ($page > 1) {
+				echo '<li class="arrow-button"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . ($page - 1) . '">&larr;</button></form></li>';
+			}
 
-    // Botones para las páginas
-    for ($n = 1; $n <= $tpages; $n++) {
-        if ($page == $n) {
-            echo '<li class="active"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
-        } else {
-            echo '<li><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
-        }
-    }
-    
-    // Botón de flecha derecha
-    if ($page < $tpages) {
-        echo '<li class="arrow-button"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . ($page + 1) . '">&rarr;</button></form></li>';
-    }
-    
-    echo '
-            </ul>
-        </div>
-    ';
-}
-?>
+			// Limita la cantidad de páginas visibles a la vez
+			$maxVisiblePages = 5;
+			$startPage = max(1, $page - floor($maxVisiblePages / 2));
+			$endPage = min($startPage + $maxVisiblePages - 1, $tpages);
+
+			// Crea botones para las páginas dentro del rango visible
+			for ($n = $startPage; $n <= $endPage; $n++) {
+				if ($page == $n) {
+					echo '<li class="active"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
+				} else {
+					echo '<li><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . $n . '">' . $n . '</button></form></li>';
+				}
+			}
+			
+			// Botón de flecha derecha si no está en la última página
+			if ($page < $tpages) {
+				echo '<li class="arrow-button"><form name="form-pages" action="" method="POST"><button type="submit" name="page" value="' . ($page + 1) . '">&rarr;</button></form></li>';
+			}
+			
+			echo '
+					</ul>
+				</div>
+			';
+		}
+		?>
+
 
 	
 
