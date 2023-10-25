bddedcb179b2aa9d920defd65676274725f4c69b
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-10-24 20:19:25 -0500

    Corrección de campos

--- v4/Sistema_LI/modules/department/form_consult.php
+++ v4/Sistema_LI/modules/department/form_consult.php
@@ -5,15 +5,29 @@ $_SESSION['id_department'] = array();
 $_SESSION['department_name'] = array();
 $_SESSION['department_description'] = array();
 
-$sql = "SELECT * FROM department WHERE id_department = '" . $_POST['txtcareer'] . "'";
+$sql = "SELECT department.id_department, department.name AS department_name, department.description, teachers.name AS teacher_name, teachers.surnames AS teacher_surname
+        FROM department 
+        LEFT JOIN teachers ON department.teacher = teachers.user 
+        WHERE department.id_department = '" . $_POST['txtcareer'] . "'";
 
 if ($result = $conexion->query($sql)) {
-	if ($row = mysqli_fetch_array($result)) {
-		$_SESSION['id_department'][0] = $row['id_department'];
-		$_SESSION['department_name'][0] = $row['name'];
-		$_SESSION['department_description'][0] = $row['description'];
-	}
+    if ($row = mysqli_fetch_array($result)) {
+        $_SESSION['id_department'][0] = $row['id_department'];
+        $_SESSION['department_name'][0] = $row['department_name'];
+        $_SESSION['department_description'][0] = $row['description'];
+
+        if ($row['teacher_name'] && $row['teacher_surname']) {
+            $_SESSION['teacher_name'][0] = $row['teacher_name'];
+            $_SESSION['teacher_surname'][0] = $row['teacher_surname'];
+        } else {
+            $_SESSION['teacher_name'][0] = 'SIN TUTOR ASIGNADO';
+            $_SESSION['teacher_surname'][0] = 'SIN TUTOR ASIGNADO';
+        }
+    }
 }
+
+
+
 
 echo '
 <div class="form-data">
@@ -31,6 +45,10 @@ echo '
 				<div class="last">
 					<label class="label">Nombre</label>
 					<input class="text" type="text" name="txtcareername" value="' . $_SESSION['department_name'][0] . '" required disabled/>
+				</div>
+				<div class="last">
+					<label class="label">Tutor a cargo</label>
+					<input class="text" type="text" name="txtcareername" value="' . $_SESSION['teacher_surname'][0] . ' ' . $_SESSION['teacher_name'][0] . '" required disabled/>
 				</div>
 				<div class="description">
 					<label class="label">Descripción</label>
