c4c7654c490e2a57fc89ffe3872563291314cefe
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-10-26 12:12:22 -0500

    Campo contraseña

--- v4/Sistema_LI/modules/administratives/update.php
+++ v4/Sistema_LI/modules/administratives/update.php
@@ -22,18 +22,33 @@ $sql = "SELECT * FROM administratives WHERE user = '" . $_POST['txtuserid'] . "'
 if ($result = $conexion->query($sql)) {
 	if ($row = mysqli_fetch_array($result)) {
 		$date = date('Y-m-d H:i:s');
-	
-		$sql_update = "UPDATE administratives SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', id = '" . trim($_POST['txtid']) . "', sede = '" . trim($_POST['selectSede']) . "', cedula = '" . trim($_POST['txtcedula']) . "', celular = '" . trim($_POST['txtcelular']) . "', pass = '" . trim($_POST['txtpass']) . "', date_of_birth = '" . trim($_POST['dateofbirth']) . "',carrer = '" . trim($_POST['selectCareer']) . "', email = '" . trim($_POST['txtemail']) . "', updated_at = '" . $date . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
-
-		if (mysqli_query($conexion, $sql_update))
-			$sql_update = "UPDATE users SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', email = '" . trim($_POST['txtemail']) . "', pass = '" . trim($_POST['txtpass']) . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
-
-		if (mysqli_query($conexion, $sql_update)) {
-			Info('administratives actualizado.');
-		} else {
-			Error('Error al actualizar.');
-		}
 		
+		if (empty($_POST['txtpass'])) {
+			$sql_update = "UPDATE administratives SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', id = '" . trim($_POST['txtid']) . "', sede = '" . trim($_POST['selectSede']) . "', cedula = '" . trim($_POST['txtcedula']) . "', celular = '" . trim($_POST['txtcelular']) . "', date_of_birth = '" . trim($_POST['dateofbirth']) . "',carrer = '" . trim($_POST['selectCareer']) . "', email = '" . trim($_POST['txtemail']) . "', updated_at = '" . $date . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
+
+			if (mysqli_query($conexion, $sql_update))
+				$sql_update = "UPDATE users SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', email = '" . trim($_POST['txtemail']) . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
+
+			if (mysqli_query($conexion, $sql_update)) {
+				Info('administratives actualizado.');
+			} else {
+				Error('Error al actualizar.');
+			}
+		} else {
+			$passhash = hash("SHA256",(trim($_POST['txtpass'])));		
+			$sql_update = "UPDATE administratives SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', id = '" . trim($_POST['txtid']) . "', sede = '" . trim($_POST['selectSede']) . "', cedula = '" . trim($_POST['txtcedula']) . "', celular = '" . trim($_POST['txtcelular']) . "', pass = '" . $passhash . "', date_of_birth = '" . trim($_POST['dateofbirth']) . "',carrer = '" . trim($_POST['selectCareer']) . "', email = '" . trim($_POST['txtemail']) . "', updated_at = '" . $date . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
+
+			if (mysqli_query($conexion, $sql_update))
+				$sql_update = "UPDATE users SET name = '" . trim($_POST['txtname']) . "', surnames = '" . trim($_POST['txtsurnames']) . "', email = '" . trim($_POST['txtemail']) . "', pass = '" . $passhash . "' WHERE user = '" . trim($_POST['txtuserid']) . "'";
+
+			if (mysqli_query($conexion, $sql_update)) {
+				Info('administratives actualizado.');
+			} else {
+				Error('Error al actualizar.');
+			}
+		
+		}
+
 		header('Location: /modules/administratives');
 		exit();
 	} else {
