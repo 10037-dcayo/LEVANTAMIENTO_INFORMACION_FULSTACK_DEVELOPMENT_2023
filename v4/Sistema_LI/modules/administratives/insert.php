a755e63cde0e9702a4618a58f4ac3544c82a7e6e
JuanMolinaT <jcmolina7@espe.edu.ec>
2023-10-26 12:11:48 -0500

    Campo contraseña

--- v4/Sistema_LI/modules/administratives/insert.php
+++ v4/Sistema_LI/modules/administratives/insert.php
@@ -6,6 +6,7 @@ include_once '../notif_info_msgbox.php';
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin.php');
 
 $_POST['txtuserid'] = trim($_POST['txtuserid']);
+$passhash = hash("SHA256",(trim($_POST['txtpass'])));
 
 if (empty($_POST['txtuserid'])) {
 	header('Location: /');
@@ -27,10 +28,10 @@ if ($result = $conexion->query($sql)) {
 	} else {
 		$date = date('Y-m-d H:i:s');
 
-		$sql_insert_user = "INSERT INTO users(user, name, surnames, email, pass, permissions, rol, image, created_at) VALUES('" . trim($_POST['txtuserid']) . "','" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['txtemail']) . "', '" . trim($_POST['txtpass']) . "', 'admin', 'admin', 'user.png','" . $date . "')";
+		$sql_insert_user = "INSERT INTO users(user, name, surnames, email, pass, permissions, rol, image, created_at) VALUES('" . trim($_POST['txtuserid']) . "','" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['txtemail']) . "', '" . $passhash . "', 'admin', 'admin', 'user.png','" . $date . "')";
 
 		if (mysqli_query($conexion, $sql_insert_user)) {
-			$sql_insert_administratives = "INSERT INTO administratives(user, name, surnames, date_of_birth, cedula, id, carrer, sede, email, celular, pass, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['dateofbirth']) . "', '" . trim($_POST['txtcedula']) . "', '" . trim($_POST['txtid']) . "', '" . trim($_POST['selectCareer']) . "', '" . trim($_POST['selectSede']) . "', '" . trim($_POST['txtemail']) . "', '" . trim($_POST['txtcelular']) . "', '" . trim($_POST['txtpass']) . "', '" . $date . "')";
+			$sql_insert_administratives = "INSERT INTO administratives(user, name, surnames, date_of_birth, cedula, id, carrer, sede, email, celular, pass, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['dateofbirth']) . "', '" . trim($_POST['txtcedula']) . "', '" . trim($_POST['txtid']) . "', '" . trim($_POST['selectCareer']) . "', '" . trim($_POST['selectSede']) . "', '" . trim($_POST['txtemail']) . "', '" . trim($_POST['txtcelular']) . "', '" . $passhash . "', '" . $date . "')";
 
 			if (mysqli_query($conexion, $sql_insert_administratives)) {
 				Info('Administrador agregado correctamente.');
